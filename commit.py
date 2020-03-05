0e231c41bc05b65d299937af19fe22b864d42759
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-03-05 07:49:40 -0500

    Edit README.md

--- commit.py
+++ commit.py
@@ -6,12 +6,15 @@ cp = cmd.run("git add .", check=True, shell=True)
 
 response = input("Do you want to use the default message for this commit?([y]/n)\n")
 
+default = "Edit README.md"
+
 if response.startswith('n'):
     message = input("What message you want?\n")
+    # Use deafult_message when user mistakenly use enter here again.
     if not message:
-        message = "Edit README.md"
+        message = deafult
 else:
-    message = "Edit README.md"
+    message = default
 
 cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
